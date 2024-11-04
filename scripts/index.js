d0afef11241a5a2d9bff30671115f1079ce2e8df
Jordan Anderson <justjordan36@gmail.com>
2024-11-04 01:19:19 -0500

    Project update

---
+++ scripts/index.js
@@ -0,0 +1,41 @@
+const initialCards = [
+    {
+    name: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+    },
+    {
+        name: "Lake Louise",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+    },
+    {
+        name: "Bald Mountains",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+    },
+    {
+        name: "Latemar",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+    },
+    {
+        name: "Vanoise National Park",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+    },
+    {
+        name: "Lago di Braies",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+    }
+];
+
+// Open the modal
+const profileEditBtn = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+
+profileEditBtn.addEventListener("click", () => {
+    profileEditModal.classList.add('modal__opened');
+});
+
+const profileCloseButton = document.querySelector("#profile-close-btn");
+
+// Close the modal
+profileCloseButton.addEventListener("click", () => {
+    profileEditModal.classList.remove("modal__opened");
+});
